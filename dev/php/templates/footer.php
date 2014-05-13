83d059f40ed7cf11aadace0914c78292f333670a
dr-potato <ismay@lokaalgevonden.nl>
2014-05-13 14:45:29 +0200

    Rendered project

---
+++ dev/php/templates/footer.php
@@ -0,0 +1,20 @@
+	<!-- Copyright info -->
+	<p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?></p>
+	
+  <!-- Load jquery from google CDN if possible, with fallback to local version -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/standalone/jquery.min.js"><\/script>')</script>
+
+	<!-- Footer javascript below here -->
+  <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/production.min.js"></script>
+
+  <!-- Livereload (remove in production!) -->
+  <script src="http://localhost:35729/livereload.js"></script>
+
+  <!-- Check if javascript for threaded comments should be loaded -->
+  <?php include 'includes/comment-check.php'; ?>
+
+	<!-- Wordpress footer -->
+	<?php wp_footer(); ?>
+	</body>
+</html>
